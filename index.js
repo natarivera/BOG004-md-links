33958444887b19f9bc1613d7097c48834a0f14ab
Nataly Escobar <natalyescobar85@gmail.com>
2022-04-25 17:05:32 -0500

    Funcion y test que valida si el Path existe

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-module.exports = () => {
-  // ...
+const fs = require('fs');
+const path = require('path');
+
+const pathExists = function(originPath){  
+  const exists = fs.existsSync(originPath);
+  console.log(`pathExists(${originPath}) ${exists}`);
+  return exists;
+}
+
+module.exports = {
+  pathExists
 };
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -8,3 +8,25 @@ describe('mdLinks', () => {
   });
 
 });
+const {pathExists} = require('../index');
+
+describe('pathExists', () => {
+
+  it('should be a function', () => {
+    expect(typeof pathExists).toBe('function');
+  });
+  // Probando con una ruta relativa (la carpeta padre ../), la funcion debe devolver true
+  it('should return true with a relative path', () => {
+    expect(pathExists('../')).toBe(true);
+  });
+  // Probando con una ruta absoluta, la funcion debe devolver true
+  it('should return true with a absolute path', () => {
+    expect(pathExists('C:\\Users\\natyc\\md-links\\test')).toBe(true);
+  });
+  // Probando con una ruta absoluta invalida, la funcion debe devolver false
+  it('should return false with an invalid path', () => {
+    expect(pathExists('C:\\Users\\natyc\\md-links\\sss')).toBe(false);
+  });
+
+
+});
